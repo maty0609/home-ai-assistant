9cb35201f91d489c727c1d732925524f135ce46a
Matyas Prokop <mprokop@natilik.com>
2025-01-20 22:16:30 +0000

    First release

--- assistant.py
+++ assistant.py
@@ -4,10 +4,10 @@ from langchain_openai import AzureChatOpenAI
 
 
 
-os.environ["AZURE_OPENAI_API_KEY"] = "8P3eN2s5a7ci2Lzm6TgERY1bW5iYoWi4VIvjtR0bQWazJrHrGilMJQQJ99BAACfhMk5XJ3w3AAABACOGx5gz"
+os.environ["AZURE_OPENAI_API_KEY"] = ""
 
 llm = AzureChatOpenAI(
-    azure_endpoint="https://natilik-openai-sw.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-08-01-preview",
+    azure_endpoint="",
     azure_deployment="gpt-4o",
     openai_api_version="2024-08-01-preview",
 )
